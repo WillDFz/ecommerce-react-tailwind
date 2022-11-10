209312c25c6a5972f0bc9169321e4ad63a71f129
WillDFz <willwow@outlook.com>
2022-11-10 09:48:53 -0300

    Update: Newsletter desktop

--- src/components/UI/Newsletter/index.jsx
+++ src/components/UI/Newsletter/index.jsx
@@ -3,10 +3,14 @@ import styles from "./styles.module.scss";
 
 const Newsletter = () => {
     return (
-        <section className={`${styles.newsletterSection} main-foreground my-5 p-3`}>
-            <h1 className="text-lg font-bold py-2">Newsletter</h1>
-            <h5 className="mb-3">Receba nossas novidades!</h5>
-            <form>
+        <section
+            className={`${styles.newsletterSection} main-foreground my-5 p-3`}
+        >
+            <div className="xl:max-w-xl xl:mx-auto">
+                <h1 className="text-lg font-bold py-2">Newsletter</h1>
+                <h5 className="mb-3">Receba nossas novidades!</h5>
+            </div>
+            <form className="xl:max-w-xl xl:mx-auto">
                 <input
                     type="text"
                     name="name"
@@ -21,7 +25,11 @@ const Newsletter = () => {
                     placeholder="E-mail"
                     required
                 />
-                <input type="submit" className="w-full main-background text-center text-secondary rounded mb-3 py-3" placeholder="Enviar" />
+                <input
+                    type="submit"
+                    className="w-full main-background text-center text-secondary rounded mb-3 py-3"
+                    placeholder="Enviar"
+                />
             </form>
         </section>
     );
